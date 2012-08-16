becfd2f6e3e91416b0b3d2b15376c4675f49b445
Donn Felker <donn@donnfelker.com>
2012-08-15 23:41:47 -0700

    Initial checkin

---
+++ routes/generate.js
@@ -0,0 +1,11 @@
+
+
+/*
+ * Project generator route. 
+*/
+exports.index = function(req, res) {
+    
+    // TODO: Loop over all the files in the project directory and then change the names. 
+    res.json(200, { message : "coming soon" })
+
+}
