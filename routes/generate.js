b8111c83b0443b7cb664b9a50754bb7f184060f8
Donn Felker <donn@donnfelker.com>
2012-08-16 00:09:02 -0700

    Simple copy and send to browser as zip

--- routes/generate.js
+++ routes/generate.js
@@ -1,11 +1,47 @@
-
-
+var wrench = require('wrench'),
+    util = require('util'),
+    spawn = require('child_process').spawn;
 /*
  * Project generator route. 
 */
 exports.index = function(req, res) {
-    
-    // TODO: Loop over all the files in the project directory and then change the names. 
-    res.json(200, { message : "coming soon" })
+  
+    var destDir = process.env.PWD + '/../android-bootstrap-2';
+
+    // Copy the files. 
+    wrench.copyDirSyncRecursive(process.env.PWD + '/../android-bootstrap', destDir);
+
+    sendZipToResponse(res, destDir);
+    console.log(process.env);
+
+}
+
+function sendZipToResponse(res, dirToZip) {
+
+  // Options -r recursive - redirect to stdout
+  var zip = spawn('zip', ['-rD', '-', dirToZip]);
+
+  res.contentType('zip');
+
+  // Keep writing stdout to res
+  zip.stdout.on('data', function (data) {
+      res.write(data);
+  });
+
+  zip.stderr.on('data', function (data) {
+      // Uncomment to see the files being added
+      console.log('zip stderr: ' + data);
+  });
+
+  // End the response on zip exit
+  zip.on('exit', function (code) {
+      if(code !== 0) {
+          res.statusCode = 500;
+          console.log('zip process exited with code ' + code);
+          res.end();
+      } else {
+          res.end();
+      }
+  });
 
 }
